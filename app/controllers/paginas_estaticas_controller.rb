928710cc826e472e994844f39f45bcce0346ff53
Marco <marco_bonetti@uol.com.br>
2018-05-12 02:57:06 +0000

    importando os dados do site

--- app/controllers/paginas_estaticas_controller.rb
+++ app/controllers/paginas_estaticas_controller.rb
@@ -1,10 +1,38 @@
 class PaginasEstaticasController < ApplicationController
   def home
-    require 'open-uri'
-    url = URI.parse('http://vangoghletters.org/vg/letters/RM01/translation.html')
-    open(url) do |http|
-        response = http.read
-        @dados = "response: #{response.inspect}"
-    end
+        require 'open-uri'
+        @dados = Array.new
+    (1..25).each do |n|
+        if n < 10
+            @url = URI.parse("http://vangoghletters.org/vg/letters/RM0#{n.to_s}/translation.html")
+          elsif n < 100
+           @url = URI.parse("http://vangoghletters.org/vg/letters/RM#{n.to_s}/translation.html")
+        end
+        open(@url) do |http|
+            @texto = http.read
+        end
+        	 resposta =+ ""+n.to_s+" = "+@texto.gsub(/<\/?[^>]*>/, "")
+        	 resposta.slice!('&nbsp;1r:1')
+        	 resposta.slice!('top')
+        	 @dados << resposta
+      end
+      
+    (1..2).each do |n|
+        if n < 10
+            @url = URI.parse("http://vangoghletters.org/vg/letters/let00#{n.to_s}/translation.html")
+          elsif n < 100
+           @url = URI.parse("http://vangoghletters.org/vg/letters/let0#{n.to_s}/translation.html")
+          else
+            @url = URI.parse("http://vangoghletters.org/vg/letters/let#{n.to_s}/translation.html")
+        end
+        open(@url) do |http|
+            @texto = http.read
+        end
+        	 resposta =+ ""+n.to_s+" = "+@texto.gsub(/<\/?[^>]*>/, "")
+        	 resposta.slice!('&nbsp;1r:1')
+        	 resposta.slice!('top')
+        	 @dados << resposta
+      end
   end
 end
+
