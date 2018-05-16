ff52bcc671b57f816d02f8a89e45a74d980a4a8c
Marco <marco_bonetti@uol.com.br>
2018-05-16 14:35:29 +0000

    cartas importadas

--- app/controllers/paginas_estaticas_controller.rb
+++ app/controllers/paginas_estaticas_controller.rb
@@ -1,38 +1,54 @@
 class PaginasEstaticasController < ApplicationController
   def home
         require 'open-uri'
-        @dados = Array.new
+        @dados = [[],[]]
+#        arquivos = ['/artwork.html','/facsimile.html','/letter.html','/line_endings.html','/notes.html','/original_text.html','/print.html','/translation.html']
+        variavel = '/translation.html'
     (1..25).each do |n|
+#    (3..5).each do |n|
         if n < 10
-            @url = URI.parse("http://vangoghletters.org/vg/letters/RM0#{n.to_s}/translation.html")
+            @url = URI.parse("http://vangoghletters.org/vg/letters/RM0#{n.to_s}#{variavel}")
           elsif n < 100
-           @url = URI.parse("http://vangoghletters.org/vg/letters/RM#{n.to_s}/translation.html")
+           @url = URI.parse("http://vangoghletters.org/vg/letters/RM#{n.to_s}#{variavel}")
         end
         open(@url) do |http|
             @texto = http.read
         end
-        	 resposta =+ ""+n.to_s+" = "+@texto.gsub(/<\/?[^>]*>/, "")
+        	 resposta =+ "-------------------------"+n.to_s+" = "+@texto.gsub(/<\/?[^>]*>/, "")
         	 resposta.slice!('&nbsp;1r:1')
         	 resposta.slice!('top')
-        	 @dados << resposta
+#        	 resposta = resposta.gsub("\n", "<br>")
+        	 @dados[0][n] = resposta
       end
-      
-    (1..2).each do |n|
+        CSV.open("./rms.csv", "wb") do |csv|
+            while(row = @dados.pop)
+                csv << row
+            end
+        end
+        @dados = [[],[]]
+#    (1..902).each do |n|
+    (702..902).each do |n|
         if n < 10
-            @url = URI.parse("http://vangoghletters.org/vg/letters/let00#{n.to_s}/translation.html")
+            @url = URI.parse("http://vangoghletters.org/vg/letters/let00#{n.to_s}#{variavel}")
           elsif n < 100
-           @url = URI.parse("http://vangoghletters.org/vg/letters/let0#{n.to_s}/translation.html")
+           @url = URI.parse("http://vangoghletters.org/vg/letters/let0#{n.to_s}#{variavel}")
           else
-            @url = URI.parse("http://vangoghletters.org/vg/letters/let#{n.to_s}/translation.html")
+            @url = URI.parse("http://vangoghletters.org/vg/letters/let#{n.to_s}#{variavel}")
         end
         open(@url) do |http|
             @texto = http.read
         end
-        	 resposta =+ ""+n.to_s+" = "+@texto.gsub(/<\/?[^>]*>/, "")
+        	 resposta =+ "-------------------------"+n.to_s+" = "+@texto.gsub(/<\/?[^>]*>/, "")
         	 resposta.slice!('&nbsp;1r:1')
         	 resposta.slice!('top')
-        	 @dados << resposta
+        	 @dados[0][n] = resposta
       end
+        CSV.open("./leters.csv", "wb") do |csv|
+            while(row = @dados.pop)
+                csv << row
+            end
+        end
+      
   end
 end
 
